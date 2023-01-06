c6624ce9e3bf142f98cdc0e62956d2535fabd89c
quangph22821 <quanghxph22821@fpt.edu.vn>
2023-01-06 21:09:06 +0700

    Toan tu

--- Array Object/object.js
+++ Array Object/object.js
@@ -44,22 +44,55 @@ restaurant.oderDelivery({
 });
 
 //
-const {name, openHours,categories} = restaurant;
-console.log(name, openHours, categories);
+// const {name, openHours,categories} = restaurant;
+// console.log(name, openHours, categories);
 
-const {menu =[], starterMenu: starters= [] }  =restaurant;
-console.log(menu,starters);
+// const {menu =[], starterMenu: starters= [] }  =restaurant;
+// console.log(menu,starters);
 
 // 
-let a = 111;
-let b = 200;
-const obj = {a:23,b:7,c:14};
-({a,b}= obj);
-console.log(a,b);
+// let a = 111;
+// let b = 200;
+// const obj = {a:23,b:7,c:14};
+// ({a,b}= obj);
+// console.log(a,b);
 
 // đối tượng lồng nhau
 
-const {
-    fri : {open:o, close:c},
-} = openHours;
-console.log(o,c);
+// const {
+//     fri : {open:o, close:c},
+// } = openHours;
+// console.log(o,c);
+
+// Object
+// const newRestaurants = {foundedIn : 1998, ...restaurant, founder: 'Beer'};
+// console.log(newRestaurants);
+
+// const restaurantCopy = {...restaurant};
+// restaurantCopy.name = 'Fish Food';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+const [a,b, ...others] = [1,2,3,4,5];
+console.log(a,b,others);
+
+const [fruit, drink, ...otherMenu ] = [
+    ...restaurant.menu,
+    ...restaurant.categories,
+] ;
+console.log(fruit, drink, otherMenu);
+
+const {sat, ...weekdays} = restaurant.openHours;
+console.log(sat, weekdays);
+
+//2. Functions
+const add = function(...number){
+    let sum = 0;
+    for(let i=0; i<number.length ;i++)
+        sum += number[i];
+        console.log(sum);
+};
+
+add(2,3);
+add(1,4,5,6);
+add(2,3,4,5,7,89);
---
+++ Toán tử/index.js
@@ -0,0 +1,92 @@
+const restaurant ={
+    name : 'Chicken Food',
+    location :'Ha Nam, Viet Nam',
+    categories : ['Chicken','Pizza','Drink','Vegetables'],
+    menu : [' Roasted Chicken ','Boiled chicken','Fruit','Drinks'],
+    openHours:{
+        thu:{
+            open: 12,
+            close :22 ,
+        },
+
+        fri:{
+            open: 11,
+            close: 23,
+        },
+        sat :{
+            open :0,
+            close :24,
+        },
+    },
+    order: function(starIndex, mainIndex){
+        return[this.categories[starIndex],this.menu[mainIndex]];
+    },
+
+    oderDelivery: function({starIndex,mainIndex,time,address}){
+        console.log(`Tôi oder ${this.categories[starIndex]} và 
+        ${this.menu[mainIndex]} tại ${address} lúc ${time}
+        `);
+    },
+    
+    oderChiken: function(mainIngredient,...otherIngredient){
+        console.log(mainIngredient);
+        console.log(otherIngredient);
+    }
+    
+};
+
+// restaurant.oderDelivery({
+//     time: '22:00',
+//     address :'Ha Noi',
+//     mainIndex:1,
+//     starIndex : 1,
+// });
+
+// restaurant.oderDelivery({
+//     address:'Ha Nam',
+//     starIndex: 2,
+// });
+// ----------1 Toán tử OR------------
+// toán tử trả về 1 giá trị trung thực đứng đầu
+// console.log('Quang' || 3);
+
+
+// console.log('' || 'Quang');
+// console.log(true || 0);
+// console.log(undefined || null); 
+
+/*
+    KHÔNG HOẠT ĐỘNG KHI BIẾN BẰNG 0
+    ?: kiểm tra xem tồn tại k
+    nếu tồn tại trả về đúng nếu k trả về 10
+*/ 
+// restaurant.numGuests = 0; 
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 15;
+// console.log(guests2);
+
+
+//-------------2. Toán tử AND-------------
+// Toán tử AND đêù đúng khi tất cả các toán hạng đều đúng
+// chỉ cần 1 hoạt động true là trả về kq sai đó
+// console.log('------AND-----');
+// console.log(0 && 'Quang');
+// console.log(7 && 'quang');
+
+// console.log('Hello' && 23 && null && 'Quang');
+
+// if(restaurant.oderChiken){
+//     restaurant.oderChiken('Food','Drink and COCA');
+// }
+
+// restaurant.oderChiken && restaurant.oderChiken('Fanta','CoCa','Sprite');
+
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests || 15;
+console.log(guests2);
+
+// Toán tử kết hợp : null and undefined (k nhận 0 và chuỗi rỗng)
+const guestCorrect = restaurant.numGuests ?? 0;
+console.log(guestCorrect);
