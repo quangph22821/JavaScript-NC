ec763d96da1f66f13f50468a1a447313629ba997
quangph22821 <quanghxph22821@fpt.edu.vn>
2023-01-29 20:03:45 +0700

    Lab2

---
+++ Lab2/lab2.1.js
@@ -0,0 +1,31 @@
+const gameEvents = new Map([
+    [17,'GOAL'],
+    [36,'Substitution'],
+    [47,'GOAL'],
+    [61,'Substitution'],
+    [64,'Yellow card'],
+    [69,'Red card'],
+    [70,'Substitution'],
+    [72,'Substitution'],
+    [76,'GOAL'],
+    [80,'GOAL'],
+    [92,'Yellow card'],
+
+]);
+
+//1.
+const events = [...new Set (gameEvents.values())]
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+
+//4.
+
+for(const [min, event] of gameEvents){
+    const  half = min <= 45 ? 'FIRST' :'SECOND';
+    console.log(`[${half}HALF] ${min}:${event}`);
+}
