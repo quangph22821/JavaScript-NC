a8fc3044e07b32bf4112493dfe928c371408d945
quangph22821 <quanghxph22821@fpt.edu.vn>
2023-02-03 11:44:30 +0700

    String

--- String/part2.js
+++ String/part2.js
@@ -23,3 +23,26 @@ lizeName('hoàng xuân quang');
 lizeName('hoàng xuân quang là tên tôi');
 
 // cách đệm 1 chuỗi
+// padStart và padEnd đệm thêm vào chuỗi ở trc và sau
+const message = 'Go to gate 23';
+
+console.log(message.padStart(20,'-').padEnd(25,'+'));
+
+const maskCard = function(number){
+    const str = number + '';
+    const last = str.slice(-2);
+    return  last.padStart(str.length,'*');
+};
+
+// console.log(maskCard(0392651003));
+
+//Ham lap lai
+const  message2 = 'Thời tiết xấu...Tất cả chuyến bay bị hoãn';
+console.log(message2.repeat(5));
+
+const plane = function(n){
+    console.log(`Có ${n} máy bay ${'2'.repeat(n)}`);
+
+};
+
+plane(5);
